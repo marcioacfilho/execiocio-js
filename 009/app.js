695c557c4c3b02a52089d4dc20694abe70079333
Márcio Alexandre <110576090+marcioacfilho@users.noreply.github.com>
2022-11-17 19:25:11 -0300

    finalizado até ex08

---
+++ 009/app.js
@@ -0,0 +1,6 @@
+function Somar(){
+    
+    let entrada = prompt("Digite uma data no formato DD/MM/AAAA.");
+const data = entrada.split("/");
+document.write(`Dia ${data[0]} do ${data[1]} de ${data[2]}.`);
+}
